2fe2b622ab1488d65fd18d230c7472a1a2e1dc82
Paulo Felipe <PauloFelipet15@gmail.com>
2023-03-29 13:16:05 -0300

    new pages

--- src/pages/NewTask/index.jsx
+++ src/pages/NewTask/index.jsx
@@ -15,7 +15,23 @@ function NewTask() {
             <div className="main-container">
                 <div className="main">
                     <div className="main-header">
-                        <NameToggle/>
+                        <NameToggle />
+
+                    </div>
+
+                    <div className="createtask-container">
+                        <h1>Criar Tarefa</h1>
+                        <div className="createtask-inputs">
+                            <label htmlFor="">*TITULO</label><input type="text" placeholder='   Ex: Mudança de layout grid para Flex' />
+
+                            <label htmlFor="">DESCRIÇÃO</label><input type="text" />
+                            <div className="createtask-inputInline">
+                                <label htmlFor="">*REPOSITÓRIO</label> <input className='repo-style' type="text" placeholder='  Ex: https://github.com/PFelipe15/meuportifolio' />
+                                <label htmlFor="">TAGS</label> <input type="text" placeholder='Ex: #HTML' />
+                            </div>
+
+                            <button>CRIAR TAREFA</button>
+                        </div>
 
                     </div>
                 </div>

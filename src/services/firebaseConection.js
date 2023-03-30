d40a8acb14bb2446e0ea83ff98c5612873ead742
Paulo Felipe <PauloFelipet15@gmail.com>
2023-03-29 21:52:18 -0300

    COMMIT NEW BALANCE

--- src/services/firebaseConection.js
+++ src/services/firebaseConection.js
@@ -1,19 +1,26 @@
 import { getAuth } from 'firebase/auth'
 import { initializeApp } from "firebase/app";
- 
+
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCsQPutKM72e7IyjLe9BM7bW-N0oSAFxiA",
-  authDomain: "dashtasksteam.firebaseapp.com",
-  projectId: "dashtasksteam",
-  storageBucket: "dashtasksteam.appspot.com",
-  messagingSenderId: "353097192095",
-  appId: "1:353097192095:web:4ad51587567c13d1b588aa"
+  // apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  // authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  // projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  // appId: import.meta.env.VITE_FIREBASE_APPID
+  apiKey: "AIzaSyAd31x5xNcZuzAh9ahtxTOYu2aCy22k3Dg",
+  authDomain: "dashtaskteam.firebaseapp.com",
+  projectId: "dashtaskteam",
+  storageBucket: "dashtaskteam.appspot.com",
+  messagingSenderId: "82795153879",
+  appId: "1:82795153879:web:b17945931eebfaee6ae392"
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app)
-export { auth, app, db}
+export { auth, app, db }
 
